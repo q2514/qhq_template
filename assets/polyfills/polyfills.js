97d32b814293275ee92dc2abf7c57041af590f55
qhq <q2514@qq.com>
2013-11-09 06:00:54 +0800

    增加IE6 png透明插件

--- assets/polyfills/polyfills.js
+++ assets/polyfills/polyfills.js
@@ -1,14 +1,17 @@
-(function() {
+//CSS3 pie 可以让IE 6 7 8 9支持部分圆角，阴影，渐变背景功能,修复IE6 PNG24透明度。
+/*(function() {
 	Modernizr.load({
   test: Modernizr.borderradius && Modernizr.boxshadow && Modernizr.cssgradients,
-  nope: '/assets/polyfills/pie/pie.1.0.js',
+  nope: 'assets/polyfills/pie/pie.1.0.js',
   callback: function () {
     if (window.PIE) {
-        $('.pie').each(function() {
+        $('.fix_ie_css3').each(function() {
             PIE.attach(this);
         });
     }
   }
 });
-
 }());
+*/
+
+

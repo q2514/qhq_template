803db8c90e5a3a037d9fc49ebff5766a173c14c0
q2514 <q2514@qq.com>
2013-11-26 14:00:54 +0800

    增加placeholders插件

--- assets/polyfills/polyfills.js
+++ assets/polyfills/polyfills.js
@@ -1,4 +1,4 @@
-/*yepnope的ie!prefix的插件用来判断IE版本(ie, ie5, ie6, ie7, ie8, ie9, iegt5, iegt6, iegt7, iegt8, ielt7, ielt8, ielt9,, ielt10)*/
+/*yepnope的ie!prefix的插件用来判断IE版本(ie, ie5, ie6, ie7, ie8, ie9, iegt5, iegt6, iegt7, iegt8, ielt7, ielt8, ielt9, ielt10)*/
 
 //CSS3 pie 可以让IE 6 7 8 9支持部分圆角，阴影，渐变背景功能,修复IE6 PNG24背景透明度。
 yepnope({
@@ -22,6 +22,12 @@ yepnope({
   }
 
 });
+
+//修复IE9以下placeholders问题。
+yepnope({
+  load: 'ielt9!assets/polyfills/placeholders/placeholders.jquery.min.js'
+});
+
 
 //修复IE9以下版本不支持Canvas 的问题。
 /*yepnope({
